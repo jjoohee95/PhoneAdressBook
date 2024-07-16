822a42ee1b0d512f74bb385120019be183d10c27
jjoohee <leejh950417@naver.com>
2024-07-16 16:33:58 +0900

    [ NEW ]PhoneAdressBook Project close # 1

---
+++ NBC_iOS4th_LJH_PhoneAddressBook/Model/UserData.swift
@@ -0,0 +1,8 @@
+//
+//  UserData.swift
+//  NBC_iOS4th_LJH_PhoneAddressBook
+//
+//  Created by t2023-m0023 on 7/16/24.
+//
+
+import Foundation
---
+++ NBC_iOS4th_LJH_PhoneAddressBook/View/MainVC.swift
@@ -0,0 +1,8 @@
+//
+//  MainVC.swift
+//  NBC_iOS4th_LJH_PhoneAddressBook
+//
+//  Created by t2023-m0023 on 7/16/24.
+//
+
+import Foundation
